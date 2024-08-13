e3a50b11bf6e082739a6f7ad480efa8ceb8de317
GoJiMin <gojimin3095@gmail.com>
2024-08-14 02:02:30 +0900

    deploy: nvm 초기화

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
 PROJECT_ROOT="/home/ubuntu/deploy"
 PROJECT_NAME="Burgerput_Chat_Server"
 
