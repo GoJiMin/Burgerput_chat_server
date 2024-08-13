7127f96b004cee475a7bc31aa22f5fec4923fd7f
GoJiMin <gojimin3095@gmail.com>
2024-08-14 03:25:12 +0900

    chore: deploy 폴더의 권한 변경

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,8 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
+sudo chown -R ubuntu:ubuntu /home/ubuntu/deploy
+
 PROJECT_ROOT="/home/ubuntu/deploy"
 PROJECT_NAME="Burgerput_Chat_Server"
 
