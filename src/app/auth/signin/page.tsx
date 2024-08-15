1481c87a19afd4698381d24afb942305ccaf92f5
GoJiMin <gojimin3095@gmail.com>
2024-08-15 22:26:31 +0900

    feat: 로그인 페이지 구현
    - token이 존재하지 않아 로그인 페이지로 리다이렉트 됐을 때 폼을 이용해 로그인합니다.
    - 로그인 정보는 ref로 관리합니다.

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,11 +1,52 @@
 "use client";
 
-import { getProviders, signIn } from "next-auth/react";
+import { signIn } from "next-auth/react";
+import { ChangeEvent, FormEvent, useRef } from "react";
 
 export default function SignPage() {
+  const formData = useRef({
+    id: "",
+    password: "",
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = e.target;
+
+    formData.current = {
+      ...formData.current,
+      [id]: value,
+    };
+  };
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    const { id, password } = formData.current;
+
+    signIn("credentials", {
+      id,
+      password,
+      callbackUrl: "http://localhost:8080",
+    });
+  };
+
   return (
     <section>
-      <button onClick={() => signIn()}>로그인</button>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          id="id"
+          value={formData.current.id}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          id="password"
+          value={formData.current.password}
+          onChange={handleChange}
+        />
+        <button>로그인</button>
+      </form>
     </section>
   );
 }
