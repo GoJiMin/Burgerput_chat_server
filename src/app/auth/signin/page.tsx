38795737f4709875d9eb79b78320a6957d19c640
GoJiMin <gojimin3095@gmail.com>
2024-08-16 00:40:42 +0900

    style: 폰트 적용 및 로그인 페이지 스타일 적용

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,10 +1,14 @@
 "use client";
 
 import { signIn } from "next-auth/react";
-import { ChangeEvent, FormEvent, useRef } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
+import { SiBurgerking } from "react-icons/si";
+
+const inputStyle =
+  "w-full outline-none border border-gray-300 focus:border-orange-500 focus:border-1 rounded-md text-md md:text-lg p-3";
 
 export default function SignPage() {
-  const formData = useRef({
+  const [formData, setFormData] = useState({
     id: "",
     password: "",
   });
@@ -12,16 +16,13 @@ export default function SignPage() {
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { id, value } = e.target;
 
-    formData.current = {
-      ...formData.current,
-      [id]: value,
-    };
+    setFormData((prev) => ({ ...prev, [id]: value }));
   };
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
 
-    const { id, password } = formData.current;
+    const { id, password } = formData;
 
     signIn("credentials", {
       id,
@@ -31,21 +32,33 @@ export default function SignPage() {
   };
 
   return (
-    <section>
-      <form onSubmit={handleSubmit}>
+    <section className="h-full flex flex-col justify-center items-center">
+      <SiBurgerking className="mb-[50px] text-[140px] md:text-[170px] text-orange-500" />
+      <form
+        className="min-w-[270px] md:min-w-[320px] flex flex-col gap-5"
+        onSubmit={handleSubmit}
+      >
         <input
+          className={inputStyle}
           type="text"
           id="id"
-          value={formData.current.id}
+          value={formData.id}
           onChange={handleChange}
+          placeholder="아이디를 입력해주세요."
+          autoComplete="off"
+          autoFocus
         />
         <input
+          className={inputStyle}
           type="password"
           id="password"
-          value={formData.current.password}
+          value={formData.password}
           onChange={handleChange}
+          placeholder="비밀번호를 입력해주세요."
         />
-        <button>로그인</button>
+        <button className="bg-orange-500 text-white w-full text-md md:text-lg p-2.5 rounded-md font-semibold">
+          로그인
+        </button>
       </form>
     </section>
   );
